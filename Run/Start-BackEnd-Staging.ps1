f1f4f2056b9f716f202af92e8c96ac82e46664e0
Pedro Vila Verde <pvilaverde@ultragenyx.com>
2023-02-15 15:42:50 +0000

    update ps

--- Run/Start-BackEnd-Staging.ps1
+++ Run/Start-BackEnd-Staging.ps1
@@ -1,5 +1,5 @@
-docker pull mrgreenondev/webaplicationtest:Dev
+docker pull mrgreenondev/webaplicationtest:Test
 docker stop MyWebCenas-Test
 docker rm MyWebCenas-Test
-docker run --name MyWebCenas-Test --env=ASPNETCORE_ENVIRONMENT=Development --env=DOTNET_RUNNING_IN_CONTAINER=true -p 4430:443 -p 8080:80 -d docker.io/mrgreenondev/webaplicationtest:Dev 
+docker run --name MyWebCenas-Test --env=ASPNETCORE_ENVIRONMENT=Staging --env=DOTNET_RUNNING_IN_CONTAINER=true -p 4430:443 -p 8080:80 -d docker.io/mrgreenondev/webaplicationtest:Test
 start-process "http://localhost:8080"
